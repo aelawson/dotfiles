df1698a557af04108c6bbc7d32a6159320b26952
Andrew Lawson <alawson@jwplayer.com>
2018-09-14 17:29:10 -0400

    Make zsh default shell in setup. Add README.

---
+++ README.md
@@ -0,0 +1,10 @@
+## Dotfiles
+
+### Installation
+
+Run `setup.sh`. This will install Homebrew, core homebrew packages, and setup ZSH.
+macOS packages installed:
+* Slack
+* Firefox
+* Spotify
+* Focus
--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 # Setup Homebrew
+echo 'Installing HomeBrew...'
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo 'Complete'
 
 brew update
 
 # Install core packages
+echo 'Installing core packages...'
 brew install zsh
 brew install htop
 brew install wget
@@ -27,8 +30,10 @@ brew install httpie
 brew tap-pin dbcli/tap
 brew install mycli
 brew install pgcli
+echo 'Complete'
 
 # Install macOS packages
+echo 'Installing macOS packages...'
 brew install cask
 brew install iterm2
 brew install firefox
@@ -37,17 +42,25 @@ brew install slack
 brew install focus
 brew install 1password-cli
 brew install lastpass-cli --with-pinentry
+echo 'Complete!'
 
 # Install global NPM packages
+echo 'Installing global NPM packages...'
 yarn global add commitizen
 yarn global add cz-conventional-changelog
+echo 'Complete!'
 
 # Setup zprezto
+echo 'Installing zprezto...'
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+echo 'Complete!'
+
+# Make ZSH default shell
+chsh -s /bin/zsh
 
 # Setup terminal
 tic -x xterm-256color-italic.terminfo
